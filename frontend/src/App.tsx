ad47210820dd19dc811b0092356321482c7197ef
RAFAEL JUNIOR DE MORAES <rafaeljrmoraes@outlook.com>
2022-05-11 13:26:02 -0300

    Clean project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>Movies ds</h1>
+            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                
+            </a>
+        </div>
+    </nav>
+</header>
+
   );
 }
 
