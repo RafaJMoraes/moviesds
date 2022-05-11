63aca16ab4cdf276d94ad41977ad47de310eeaa9
RAFAEL JUNIOR DE MORAES <rafaeljrmoraes@outlook.com>
2022-05-11 12:54:41 -0300

    create backend

---
+++ backend/src/main/java/com/moviesds/moviesds/MoviesdsApplication.java
@@ -0,0 +1,13 @@
+package com.moviesds.moviesds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/moviesds/moviesds/MoviesdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesds.moviesds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
