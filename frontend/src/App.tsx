5be5d598a3a3cedc7bd830f587055491f4138ee3
RAFAEL JUNIOR DE MORAES <rafaeljrmoraes@outlook.com>
2022-05-11 22:23:34 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import Navbar from "./components/Navbar/navbar";
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from "pages/Listing/index";
+import Form from "pages/form/index";
+import Navbar from "components/Navbar/navbar";
 
 function App() {
   return (
-   <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
