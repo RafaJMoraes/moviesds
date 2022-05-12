a1a12cb58ec79c0214b37a06590a977076bbbf83
RAFAEL JUNIOR DE MORAES <rafaeljrmoraes@outlook.com>
2022-05-11 22:04:32 -0300

    Navbar

--- frontend/src/components/Navbar/navbar.tsx
+++ frontend/src/components/Navbar/navbar.tsx
@@ -1,16 +1,17 @@
-import {ReactComponent as GitHubIcon} from "../../assets/img/github.svg"
+import {ReactComponent as GitHubIcon} from "assets/img/github.svg"
+import './styles.css'
 
 function Navbar(){
     /** Formato jsx - Html com componentes react dentro dele */
     return (
         <header>
         <nav className="container">
-            <div className="dsmovie-nav-content">
-                <h1>Movies ds</h1>
+            <div className="moviesds-nav-content">
+                <h1>Moviesds</h1>
                 <a href="https://github.com/RafaJMoraes">
-                <div>
+                <div className="moviesds-contact-name">
                   <GitHubIcon />
-                  <p>/RafaelJmoraees</p>
+                  <p className="moviesds-contact-link">/RafaelJmoraes</p>
                 </div>
                 </a>
             </div>
