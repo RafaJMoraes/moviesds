a0f20081568b73c42b2f02b241bd515374bfc8aa
RAFAEL JUNIOR DE MORAES <rafaeljrmoraes@outlook.com>
2022-05-14 22:43:21 -0300

    Save, useNavigation

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,10 +1,12 @@
 
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Movie, MoviePage } from "types/movie";
+import axios, { AxiosRequestConfig } from "axios";
+import { Movie } from "types/movie";
 import './styles.css'
 import { BASE_URL } from "utils/request";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { validateEmail } from "utils/validate";
 
 type Props = {
   movieId: String;
@@ -26,15 +28,46 @@ function FormCard({movieId} : Props) {
     
  
   
+      const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+
+        if(!validateEmail(email)){
+          return;
+        }
+
+        const config: AxiosRequestConfig = {
+          baseURL: BASE_URL,
+          method: 'PUT',
+          url: '/scores',
+          data: {
+            email: email,
+            movieId: movieId,
+            score: score
+          }
+        }
+        axios(config).then(response => {
+          
+          navigate('/');
+
+        });
+
+      };
+
+      
 
  
   return (
+
       
     <div className="dsmovie-form-container">
       <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
+        <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
@@ -55,7 +88,10 @@ function FormCard({movieId} : Props) {
             </button>
           </div>
         </form>
-       
+        <Link to='/'>
+           <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
+     
         
       </div>
     </div>
